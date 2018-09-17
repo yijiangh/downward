7fc83a09c032fcf3413d83d5a1543693571e40d6
Caelan Garrett <caelan@mit.edu>
2018-09-17 09:58:00 -0400

    Scaling costs

--- src/translate/pddl/f_expression.py
+++ src/translate/pddl/f_expression.py
@@ -1,4 +1,10 @@
-class FunctionalExpression:
+from __future__ import print_function
+
+import math
+
+COST_SCALE = 1
+
+class FunctionalExpression(object):
     def __init__(self, parts):
         self.parts = tuple(parts)
     def dump(self, indent="  "):
@@ -13,11 +19,12 @@ class FunctionalExpression:
 class NumericConstant(FunctionalExpression):
     parts = ()
     def __init__(self, value):
-        if value != int(value):
-            raise ValueError("Fractional numbers are not supported")
-        self.value = int(value)
+        #if value != int(value):
+        #    raise ValueError("Fractional numbers are not supported")
+        #self.value = int(value)
+        self.value = int(math.ceil(COST_SCALE*float(value)))
     def __eq__(self, other):
-        return (self.__class__ == other.__class__ and self.value == other.value)
+        return self.__class__ == other.__class__ and self.value == other.value
     def __str__(self):
         return "%s %s" % (self.__class__.__name__, self.value)
     def _dump(self):
